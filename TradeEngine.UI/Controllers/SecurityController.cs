2b3606bfa05ee9a17ab2ea26b53e36fb46c19c9d
bvinot <bremevinoth@gmail.com>
2018-06-17 08:06:11 +0530

    MVC API

---
+++ TradeEngine.UI/Controllers/SecurityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace TradeEngine.UI.Controllers
+{
+    public class SecurityController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var prd = new List<object> { new { Thell = "sdf", Bell = "sss" }, new { Thell = "ddd", Cell = "asd" } };
+
+            return Request.CreateResponse(HttpStatusCode.NotFound, JsonConvert.SerializeObject(prd));
+        }
+    }
+}
